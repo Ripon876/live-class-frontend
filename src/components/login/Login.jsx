7f9667c526269da62ac072e17e5480570d855771
MD Ripon Islam <islam876ripon@gmail.com>
2022-12-17 16:30:38 +0600

    state added to store initial form data

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,17 +1,32 @@
-import {Link} from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
-
 function Login() {
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+	const handleChange = (e) => {
+		let value = e.target.value;
+		setFormData({
+			...formData,
+			[e.target.name]: value,
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formData);
+	};
+
 	return (
 		<div>
 			<div className="container">
 				<div className="loginForm">
-					<div
-						className='border formContainer p-4 rounded-2 shadow-sm'
-					>
+					<div className="border formContainer p-4 rounded-2 shadow-sm">
 						<form>
-						<h4 className='text-center fw-bold'>Login</h4>
+							<h4 className="text-center fw-bold">Login</h4>
 							<div className="mb-3">
 								<label htmlFor="email" className="form-label">
 									Email address
@@ -20,7 +35,8 @@ function Login() {
 									type="email"
 									className="form-control"
 									id="email"
-									aria-describedby="emailHelp"
+									name="email"
+									onChange={handleChange}
 								/>
 							</div>
 							<div className="mb-3">
@@ -34,18 +50,33 @@ function Login() {
 									type="password"
 									className="form-control"
 									id="password"
+									name="password"
+									onChange={handleChange}
 								/>
 							</div>
 							<div className="text-center">
-								<button className="btn btn-primary btnSubmit mb-2">Login</button>
-							<p className='mb-0'>Don't have an account ? <Link to='/register' className='formFooterLink'>Register now</Link></p>
+								<button
+									className="btn btn-primary btnSubmit mb-2"
+									onClick={handleSubmit}
+								>
+									Login
+								</button>
+								<p className="mb-0">
+									Don't have an account ?
+									<Link
+										to="/register"
+										className="formFooterLink"
+									>
+										Register now
+									</Link>
+								</p>
 							</div>
 						</form>
 					</div>
 				</div>
 			</div>
 		</div>
-	)
+	);
 }
 
 export default Login;
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,17 +1,32 @@
-import {Link} from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Register.css";
 
-
 function Register() {
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+	const handleChange = (e) => {
+		let value = e.target.value;
+		setFormData({
+			...formData,
+			[e.target.name]: value,
+		});
+	};
+
+const handleSubmit = (e) => {
+	e.preventDefault();
+	console.log(formData);
+}
+
 	return (
 		<div>
 			<div className="container">
 				<div className="registerForm">
-					<div
-						className='border formContainer p-4 rounded-2 shadow-sm'
-					>
+					<div className="border formContainer p-4 rounded-2 shadow-sm">
 						<form>
-						<h4 className='text-center fw-bold'>Register</h4>
+							<h4 className="text-center fw-bold">Register</h4>
 							<div className="mb-3">
 								<label htmlFor="email" className="form-label">
 									Email address
@@ -20,7 +35,8 @@ function Register() {
 									type="email"
 									className="form-control"
 									id="email"
-									aria-describedby="emailHelp"
+									name="email"
+									onChange={handleChange}
 								/>
 							</div>
 							<div className="mb-3">
@@ -34,11 +50,26 @@ function Register() {
 									type="password"
 									className="form-control"
 									id="password"
+									name="password"
+									onChange={handleChange}
 								/>
 							</div>
 							<div className="text-center">
-								<button className="btn btn-primary btnSubmit mb-2">Register</button>
-							<p className='mb-0'>Already have an account ? <Link to='/login'  className='formFooterLink'>Login now</Link></p>
+								<button 
+								className="btn btn-primary btnSubmit mb-2"
+								onClick={handleSubmit}
+								>
+									Register
+								</button>
+								<p className="mb-0">
+									Already have an account ?
+									<Link
+										to="/login"
+										className="formFooterLink"
+									>
+										Login now
+									</Link>
+								</p>
 							</div>
 						</form>
 					</div>
