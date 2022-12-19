8a3dcb370c5fc54c1da5b89d28df9425cc8537cc
MD Ripon Islam <islam876ripon@gmail.com>
2022-12-19 15:48:10 +0600

    teacher class taking component added

--- src/scenes/teacher/StartClassAsTeacher.jsx
+++ src/scenes/teacher/StartClassAsTeacher.jsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import axios from "axios";
 import { useCookies } from "react-cookie";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 function StartClassAsTeacher() {
 	const [cls, setCls] = useState({});
 	const [searchParams, setSearchParams] = useSearchParams();
 	const [cookies, setCookie] = useCookies([]);
+	const [clsStarted, setClsStarted] = useState(false);
 
 	useEffect(() => {
 		axios
@@ -23,12 +26,37 @@ function StartClassAsTeacher() {
 			.catch((err) => console.log("err :", err));
 	}, []);
 
+	const startClass = () => {
+		document.querySelector(".MuiButtonBase-root").click();
+		setClsStarted(true);
+	};
+
 	return (
 		<div style={{ overflowY: "scroll", maxHeight: "90%" }}>
-			<Box component="div" m="40px 40px " width="90%" p="0 0 0 20px">
-				<Typography variant="h4" mb="20px">
-					Start This class
-				</Typography>
+			<Box
+				component="div"
+				m="40px 40px "
+				width="90%"
+				p="0 0 0 20px"
+				align="center"
+			>
+				{!clsStarted && (
+					<>
+						<Typography variant="h3" mt="150px">
+							' {cls.title} '
+						</Typography>
+						<Typography variant="h4" mb="20px">
+							Each class will be : {cls.classDuration} min
+						</Typography>
+						<Button
+							variant="contained"
+							size="large"
+							onClick={startClass}
+						>
+							Launch
+						</Button>
+					</>
+				)}
 			</Box>
 		</div>
 	);
