a53648daddc71fa3064346ec1c16d9a0f61a6175
MD Ripon Islam <islam876ripon@gmail.com>
2022-12-21 16:54:02 +0600

    connection working without page reload

--- src/scenes/teacher/StartClassAsTeacher2.jsx
+++ src/scenes/teacher/StartClassAsTeacher2.jsx
@@ -21,10 +21,10 @@ function StartClassAsTeacher2() {
 	const [searchParams, setSearchParams] = useSearchParams();
 	const [cookies, setCookie] = useCookies([]);
 	const [clsStarted, setClsStarted] = useState(false);
+
+	// for call
 	const [calling, setCaling] = useState(false);
 	const [myStream, setMyStream] = useState();
-	// for call
-
 	const [peerId, setPeerId] = useState(searchParams.get("id"));
 	const [remotePeerIdValue, setRemotePeerIdValue] = useState("");
 	const remoteVideoRef = useRef(null);
@@ -38,8 +38,12 @@ function StartClassAsTeacher2() {
 		peer.on("open", (id) => {
 			setPeerId(id);
 		});
-
+console.log(peer)
 		peer.on("call", (call) => {
+
+console.log('calling')
+
+
 			var getUserMedia =
 				navigator.getUserMedia ||
 				navigator.webkitGetUserMedia ||
@@ -63,32 +67,29 @@ function StartClassAsTeacher2() {
 		peerInstance.current = peer;
 	}, []);
 
+	const answerCall = () => {
+		callerRef.current.answer(myStream);
+		setCaling(false);
+	};
 
-const answerCall = () => {
-	callerRef.current.answer(myStream);
-	setCaling(false);
-}
+	// fetching class
+	useEffect(() => {
+		axios
+			.get(
+				"http://localhost:5000/teacher/get-class/" +
+					searchParams.get("id"),
+				{
+					headers: { Authorization: `Bearer ${cookies.token}` },
+				}
+			)
+			.then((data) => setCls({ ...data.data.cls }))
+			.catch((err) => console.log("err :", err));
+	}, []);
 
+	const startClass = () => {
+		setClsStarted(true);
+	};
 
-
-	// const call = (remotePeerId) => {
-	// 	var getUserMedia =
-	// 		navigator.getUserMedia ||
-	// 		navigator.webkitGetUserMedia ||
-	// 		navigator.mozGetUserMedia;
-
-	// 	getUserMedia({ video: true, audio: true }, (mediaStream) => {
-	// 		currentUserVideoRef.current.srcObject = mediaStream;
-	// 		currentUserVideoRef.current.play();
-
-	// 		const call = peerInstance.current.call(remotePeerId, mediaStream);
-
-	// 		call.on("stream", (remoteStream) => {
-	// 			remoteVideoRef.current.srcObject = remoteStream;
-	// 			remoteVideoRef.current.play();
-	// 		});
-	// 	});
-	// };
 	return (
 		<div style={{ overflowY: "scroll", maxHeight: "90%" }}>
 			<Box
@@ -98,48 +99,64 @@ const answerCall = () => {
 				p="0 0 0 20px"
 				align="center"
 			>
+				{calling && (
+					<Button
+						variant="contained"
+						size="large"
+						onClick={answerCall}
+					>
+						Allow
+					</Button>
+				)}
 
+				{!clsStarted && (
+					<>
+						<Typography variant="h3" mt="150px">
+							' {cls.title} '
+						</Typography>
+						<Typography variant="h4" mb="20px">
+							Each class will be : {cls.classDuration} min
+						</Typography>
 
-{calling && <button onClick={answerCall}></button>}
+						{cls.hasToJoin !== 0 && (
+							<Button
+								variant="contained"
+								size="large"
+								onClick={startClass}
+							>
+								Start Class
+							</Button>
+						)}
+					</>
+				)}
 
+				{clsStarted && (
+					<div>
+						<div className="container">
+							<div className="video-container">
+								<div className="video myVideo">
+									<div>
+										<video
+											playsInline
+											muted
+											ref={currentUserVideoRef}
+											autoPlay
+										/>
 
-
-				<Typography variant="h3" mt="150px">
-					{peerId}
-				</Typography>
-
-				<input
-					type="text"
-					value={remotePeerIdValue}
-					onChange={(e) => setRemotePeerIdValue(e.target.value)}
-				/>
-				{/*<button onClick={() => call(remotePeerIdValue)}>Call</button>*/}
-
-				<div>
-					<div className="container">
-						<div className="video-container">
-							<div className="video myVideo">
-								<div>
+										<h2>You</h2>
+									</div>
+								</div>
+								<div className="video otherVideo">
 									<video
 										playsInline
-										muted
-										ref={currentUserVideoRef}
+										ref={remoteVideoRef}
 										autoPlay
 									/>
-
-									<h2>You</h2>
 								</div>
-							</div>
-							<div className="video otherVideo">
-								<video
-									playsInline
-									ref={remoteVideoRef}
-									autoPlay
-								/>
 							</div>
 						</div>
 					</div>
-				</div>
+				)}
 			</Box>
 		</div>
 	);
