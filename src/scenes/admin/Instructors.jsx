d1ba0036c8858c8ab09c5b3b290c0dc710b826b4
MD Ripon Islam <islam876ripon@gmail.com>
2023-01-01 15:49:22 +0600

    showing instructors from db

--- src/scenes/admin/Instructors.jsx
+++ src/scenes/admin/Instructors.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Box, Typhography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import axios from "axios";
 import { tokens } from "../../theme";
 import { mockData } from "../../data/mockdata";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
@@ -8,11 +9,22 @@ import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 import Header from "../../components/Header";
 
-
-
 const Instructors = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [instructors, setInstructors] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_SERVER_URL + "/admin/get-teachers")
+      .then((data) => {
+        data.data.teachers.map((user, i) => (user.id = i + 1));
+        setInstructors([...data.data.teachers]);
+
+        console.log(data.data.teachers);
+      })
+      .catch((err) => console.log("err :", err));
+  }, []);
 
   const columns = [
     { field: "id", headerName: "ID" },
@@ -27,7 +39,7 @@ const Instructors = () => {
       headerName: "Age",
       type: "number",
       headerAlign: "left",
-      align:"left",
+      align: "left",
     },
     {
       field: "phone",
@@ -35,17 +47,18 @@ const Instructors = () => {
       flex: 1,
     },
     {
-    field: "email",
-    headerName: "Email",
-    flex: 1
-  },
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
   ];
   return (
     <Box m="20px">
       <Header title="Instructors" subtitle="Manage The Instructors" />
 
       <Box m="40px 0 0 0" height="70vh">
-        <DataGrid rows={mockData} columns={columns} />
+        {/*<DataGrid rows={mockData} columns={columns} />*/}
+        <DataGrid rows={instructors} columns={columns} />
       </Box>
     </Box>
   );
