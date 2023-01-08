7d5e955b899175b574e9f98cdeb54327f6fc591a
MD Ripon Islam <islam876ripon@gmail.com>
2023-01-08 16:03:59 +0600

    hiding remaining time for admin

--- src/scenes/Dashboard/index.jsx
+++ src/scenes/Dashboard/index.jsx
@@ -4,14 +4,22 @@ import Header from "../../components/Header";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import RemainingTime from "./RemainingTime";
+import { useSelector } from "react-redux";
+
+
 
 const Dashboard = () => {
+
+  const userType = useSelector((state) => state.type);
+
+
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your Dashboard" />
       </Box>
-      <RemainingTime />
+      {userType  !== 'admin' &&  <RemainingTime />}
+     
     </Box>
   );
 };
