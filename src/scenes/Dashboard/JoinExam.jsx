1acf3d48754305bf90bdd3b3085ecc7955e8c2c6
MD Ripon Islam <islam876ripon@gmail.com>
2023-01-08 17:32:53 +0600

    examiner, roleplayer, candidate redirecting to there exams after admin clicks start

--- src/scenes/Dashboard/JoinExam.jsx
+++ src/scenes/Dashboard/JoinExam.jsx
@@ -2,17 +2,16 @@ import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import { useSelector } from "react-redux";
 let socket;
-
+let examId;
 function JoinExam() {
 	const user = useSelector((state) => state);
-	const [examId, setExamId] = useState("");
 	useEffect(() => {
 		socket = io.connect(process.env.REACT_APP_SERVER_URL);
 
 		socket.on("connect", () => {
 			socket.emit("getExamId", user, (err, id) => {
 				if (!err) {
-					setExamId(id);
+					examId = id;
 				} else {
 					console.log(err);
 				}
