ad20931ab8c3917c2fdf9e6522e5f261d8fc6868
MD Ripon Islam <islam876ripon@gmail.com>
2023-01-16 00:54:45 +0600

    exam details page added

---
+++ src/scenes/admin/ExamDetails.jsx
@@ -0,0 +1,13 @@
+import Box from "@mui/material/Box";
+
+function ExamDetails() {
+	return (
+		<div style={{ overflowY: "auto", maxHeight: "90%" }}>
+			<Box component="div" m="40px 40px " width="90%" p="0 0 0 20px">
+				dfdsfdsfd gfdgsfgsfdfg sfdgf
+			</Box>
+		</div>
+	);
+}
+
+export default ExamDetails;
