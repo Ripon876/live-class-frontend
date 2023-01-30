dc72890b9c1496d38851ead2d43aca73fb7e5ad2
MD Ripon Islam <islam876ripon@gmail.com>
2023-01-30 21:05:33 +0600

    meeting config + theme mode changed

--- src/scenes/MeetingComp.jsx
+++ src/scenes/MeetingComp.jsx
@@ -27,68 +27,74 @@ const MeetingComp = ({ id, title, name, apiRef, sct, ao }) => {
 	}, []);
 	return (
 		<div className="jitsiContainer">
-		<JitsiMeeting
-			configOverwrite={{
-				startWithAudioMuted: true,
-				startWithVideoMuted: true,
-				hiddenPremeetingButtons: ["microphone"],
-				prejoinPageEnabled: false,
-				logoImageUrl: "",
-				DEFAULT_LOGO_URL: "",
-				defaultLocalDisplayName: "me",
-				readOnlyName: true,
-				enableInsecureRoomNameWarning: false,
-				disableInviteFunctions: true,
-				remoteVideoMenu: {
-					disabled: true,
-				},
-				hideRecordingLabel: false,
-				disableSelfView: true,
-				hideParticipantsStats: true,
-				subject: title,
-				giphy: {
-					enabled: false,
-				},
-			}}
-			spinner={Sp}
-			interfaceConfigOverwrite={{
-				DEFAULT_BACKGROUND: "#3b98ff",
-				noSsl: true,
-				SHOW_JITSI_WATERMARK: false,
-				HIDE_DEEP_LINKING_LOGO: false,
-				SHOW_BRAND_WATERMARK: false,
-				SHOW_WATERMARK_FOR_GUESTS: false,
-				SHOW_POWERED_BY: false,
-				SHOW_CHROME_EXTENSION_BANNER: false,
-				DEFAULT_WELCOME_PAGE_LOGO_URL: "",
-				TOOLBAR_BUTTONS: [
-					"microphone",
-					"camera",
-					// "closedcaptions",
-					// "desktop",
-					"fullscreen",
-					// "settings",
-					// "raisehand",
-					// "videoquality",
-				],
-			}}
-			userInfo={{
-				displayName: name,
-			}}
-			onApiReady={(externalApi) => {
-				console.log("api ready");
+			<JitsiMeeting
+				configOverwrite={{
+					startWithAudioMuted: true,
+					startWithVideoMuted: true,
+					hiddenPremeetingButtons: ["microphone"],
+					prejoinPageEnabled: false,
+					logoImageUrl: "",
+					DEFAULT_LOGO_URL: "",
+					defaultLocalDisplayName: "me",
+					readOnlyName: true,
+					enableInsecureRoomNameWarning: false,
+					disableInviteFunctions: true,
+					remoteVideoMenu: {
+						disabled: true,
+					},
+					hideRecordingLabel: false,
+					disableSelfView: true,
+					hideParticipantsStats: true,
+					subject: title,
+					giphy: {
+						enabled: false,
+					},
+					conferenceInfo: {
+						autoHide: ["subject"],
+					},
+					hideConferenceTimer: true,
+					hideParticipantsStats: true,
+				}}
+				spinner={Sp}
+				interfaceConfigOverwrite={{
+					// DEFAULT_BACKGROUND: "#3b98ff",
+					DEFAULT_BACKGROUND: "#0e131ea3",
+					noSsl: true,
+					SHOW_JITSI_WATERMARK: false,
+					HIDE_DEEP_LINKING_LOGO: false,
+					SHOW_BRAND_WATERMARK: false,
+					SHOW_WATERMARK_FOR_GUESTS: false,
+					SHOW_POWERED_BY: false,
+					SHOW_CHROME_EXTENSION_BANNER: false,
+					DEFAULT_WELCOME_PAGE_LOGO_URL: "",
+					TOOLBAR_BUTTONS: [
+						"microphone",
+						"camera",
+						// "closedcaptions",
+						// "desktop",
+						"fullscreen",
+						// "settings",
+						// "raisehand",
+						// "videoquality",
+					],
+				}}
+				userInfo={{
+					displayName: name,
+				}}
+				onApiReady={(externalApi) => {
+					console.log("api ready");
 
-				// if (apiRef) {
-				// 	apiRef.current = externalApi;
-				// }
-				// if (sct) {
-				// 	// sct(Date.now());
-				// }
-			}}
-			getIFrameRef={(node) => (node.style.height = "600px")}
-			key={"34543df"}
-			roomName={"Station" + id}
-		/>
+					// if (apiRef) {
+					// 	apiRef.current = externalApi;
+					// }
+					// if (sct) {
+					// 	// sct(Date.now());
+					// }
+				}}
+				getIFrameRef={(node) => (node.style.height = "600px")}
+				key={"34543df"}
+				roomName={"Station" + id}
+			/>
 		</div>
 	);
 };
