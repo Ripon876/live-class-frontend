f00f3b2382f9257cd815980f187e38562cb6ee12
MD Ripon Islam <islam876ripon@gmail.com>
2023-01-31 00:52:30 +0600

    removed extra imports

--- src/global/Topbar.jsx
+++ src/global/Topbar.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import { Box, Icon, IconButton, useTheme } from "@mui/material";
-import { useContext } from "react";
-import { InputBase } from "@mui/material";
-import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import { Box, IconButton } from "@mui/material";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 
 const Topbar = () => {
   const navigate = useNavigate();
-
-  const [cookies, setCookie, removeCookie] = useCookies([]);
+  const [cookies, setCookie, removeCookie] = useCookies();
 
   const logOut = () => {
     removeCookie("token");
